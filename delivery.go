8a38770c88130d187e9707b45d74054663da4cfb
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2019-08-03 12:51:13 -0500

    no interfaces!

--- delivery.go
+++ delivery.go
@@ -6,13 +6,13 @@ type SaleRepresentation struct {
 	Name        string
 	Extension   string
 	ContentType string
-	Metadata    map[string]interface{}
+	Metadata    map[string]string
 }
 
 // DeliveryChannel a delivery channel is a way to represent and send a voucher of yourtransaction.
 // examples of delivery channel are: SMTP, SMS, ThermalPrint, etc...
 type DeliveryChannel interface {
 	Name() string
-	SendSale(from *Company, sale *Sale, metadata ...map[string]interface{}) error
-	SaleRepresentation(from *Company, sale *Sale, metadata ...map[string]interface{}) (*SaleRepresentation, error)
+	SendSale(from *Company, sale *Sale, metadata ...map[string]string) error
+	SaleRepresentation(from *Company, sale *Sale, metadata ...map[string]string) (*SaleRepresentation, error)
 }
